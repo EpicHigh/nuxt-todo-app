d185a3ab82611900c4001a68c5ac395bf9b2fc7e
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-18 16:05:11 +0700

    feat: add @vitejs/plugin-vue and jsdom to test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  test: {},
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
