377482e64b9fddd8a0af479f9ba26f0bd32f7bce
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-18 09:56:56 +0700

    feat: Add Task Manager app with components and styles

--- app.vue
+++ app.vue
@@ -1,14 +1,36 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="app">
+    <div class="app-header">
+      <h1>Task Manager</h1>
+    </div>
+    <div class="app-content">
+      <div class="container">
+        <TaskList v-if="tasks.length > 0" :tasks="tasks" />
+        <EmptyState v-else @click="showAddTaskDialog = true">
+          Add a task
+        </EmptyState>
+        <CommonDialog
+          v-if="showAddTaskDialog"
+          @close="showAddTaskDialog = false"
+        >
+          <TaskForm />
+        </CommonDialog>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import useTaskStore from '~/store/task';
+import TaskList from '~/components/containers/TaskList.vue';
+import EmptyState from '~/components/common/EmptyState.vue';
+import CommonDialog from '~/components/common/CommonDialog.vue';
+import TaskForm from '~/components/containers/TaskForm.vue';
 
 const taskStore = useTaskStore();
+const showAddTaskDialog = useState('show-dialog', () => false);
 
 await taskStore.fetchTasks();
+const { tasks } = taskStore;
 
 useHead({
   title: 'Task Manager',
@@ -39,10 +61,29 @@ useHead({
 });
 </script>
 <style lang="css">
+html {
+  font-size: 10px;
+}
+
 body {
   font-family: 'Poppins', sans-serif;
-  font-size: 16px;
+  font-size: 1.6rem;
   margin: 0;
   padding: 0;
+}
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.app-header {
+  background-color: #2b6cb0;
+  color: #fff;
+  padding: 2rem;
+  text-align: center;
+}
+.app-content {
+  flex: 1;
+  padding: 2rem;
 }
 </style>
---
+++ components/common/EmptyState.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="empty-state" v-bind="$attrs" type="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="css">
+.empty-state {
+  background-color: transparent;
+  padding: 3rem;
+  font-size: inherit;
+  color: inherit;
+  cursor: pointer;
+  border-color: rgba(209, 213, 219, 0.4);
+  border-style: dashed;
+  border-radius: 0.5rem;
+  text-align: center;
+}
+</style>
