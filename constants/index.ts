a8523de570477571d25334cc8780820797ae53ed
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-19 14:26:38 +0700

    feat: add environment variable support for BASE_URL

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL =
+  process.env.TASK_MANAGER_SERVICE_ENDPOINT || 'http://localhost:3001';
 export const GENERIC_ERROR = 'Something went wrong';
 
 export const TASKS_ENDPOINT = '/tasks';
