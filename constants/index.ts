8239f99eaa435a10d0d15135914adc383ba46160
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-06-19 16:44:03 +0700

    fix(constants): update BASE_URL for production deployment

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,8 @@
 export const BASE_URL =
-  process.env.TASK_MANAGER_SERVICE_ENDPOINT || 'http://localhost:3001';
+  process.env.NODE_ENV === 'production'
+    ? 'https://nuxt-todo-service.onrender.com'
+    : 'http://localhost:3001';
+
 export const GENERIC_ERROR = 'Something went wrong';
 
 export const TASKS_ENDPOINT = '/tasks';
